e82c9de1e626024cbe6cfccf334c0eddb69fdea8
ndjndj <teq.ndj@gmail.com>
2020-12-18 01:38:53 +0900

    learn jsx syntax.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">Hello, world!</div>
+      <React.Fragment>
+        <h1>Hello, world!</h1>
+        <p>Welcome to React.</p>
+      </React.Fragment>
+      
     );
   }
 }
