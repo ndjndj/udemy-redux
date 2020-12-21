8640e85b091fc7c227f1ef77c863912832419ace
ndjndj <teq.ndj@gmail.com>
2020-12-22 00:12:47 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, { Component } from 'react';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <h1>Hello, world!</h1>
-//         <p>Welcome to React.</p>
-//       </React.Fragment>
-//       
-//     );
-//   }
-// }
-
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "Kenta", age: 18 },
+    { name: "NoName" }
+  ];
+
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   );
 }
 
-const Cat = () => {
+const User = (props) => {
   return (
-    <div>CAT</div>
+    <div>Hi, I am {props.name}, and {props.age} years old!</div>
   );
+}
+
+User.defaultProps = {
+  age: 1,
+
 }
 
 export default App;
